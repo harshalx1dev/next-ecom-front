0181215ccfcaded931cec55ed1d567907d2fa258
Harshal <harshalx1.dev@gmail.com>
2025-08-06 00:56:09 +0530

    billboard style update

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -7,9 +7,9 @@ interface BillboardProps {
 export const Billboard = ({ data }: BillboardProps) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover" style={{backgroundImage: `url(${data?.imageUrl})`}}>
+      <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center" style={{backgroundImage: `url(${data?.imageUrl})`}}>
         <div className="h-full w-full flex flex-col justify-center items-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs" style={{color: data.labelColor}}>
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-4xl max-w-xs text-center" style={{color: data.labelColor}}>
             {data?.label}
           </div>
         </div>
