0c0d8ab978f2590614ac8203c1c29c619caa662f
Harshal <harshalx1.dev@gmail.com>
2025-08-08 00:53:43 +0530

    category id added

--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -3,7 +3,7 @@ import { Size } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`;
 
 export const getSizes = async (categoryId: string): Promise<Size[]> => {
-  const sizesRes = await fetch(URL + (categoryId ? categoryId : ''));
+  const sizesRes = await fetch(URL + (categoryId ? `?categoryId=${categoryId}` : ''));
   const body = await sizesRes.json();
   return body.data;
 }
